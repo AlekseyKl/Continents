9916e624ffb8b4bd24996df1c5a6eae9fb46075a
root <root@9574.ovz-n19.hc.ru>
2016-02-23 12:52:21 +0300

    create base template

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('ContinentsBundle:Default:index.html.twig');
+        return $this->render('ContinentsBundle:Default:index.html.twig', ["window"=>"please, enter your berthday"]);
     }
 }
